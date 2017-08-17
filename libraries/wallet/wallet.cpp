c080c8bf467602bed4cb7144fe4f1e7a4212cdd9
Alexander Morris <qd01a@yourika.com>
2017-08-17 16:20:03 -0400

    Update wallet.cpp

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -1933,9 +1933,11 @@ public:
                                  string symbol_to_sell,
                                  string min_to_receive,
                                  string symbol_to_receive,
+								         fc::optional<graphene::chain::limit_order_create_operation::limit_order_flags>  order_flags,
                                  uint32_t timeout_sec = 0,
                                  bool   fill_or_kill = false,
-                                 bool   broadcast = false)
+                                 bool   broadcast = false
+								 )
    {
       account_object seller   = get_account( seller_account );
 
@@ -1946,6 +1948,12 @@ public:
       if( timeout_sec )
          op.expiration = fc::time_point::now() + fc::seconds(timeout_sec);
       op.fill_or_kill = fill_or_kill;
+
+	   if (order_flags && order_flags.valid()) {
+			extension<graphene::chain::limit_order_create_operation::limit_order_flags> ext;
+			ext.value = *order_flags;
+			op.extensions = ext;
+	   }
 
       signed_transaction tx;
       tx.operations.push_back(op);
@@ -3773,12 +3781,13 @@ signed_transaction wallet_api::sell_asset(string seller_account,
                                           string symbol_to_sell,
                                           string min_to_receive,
                                           string symbol_to_receive,
+										            fc::optional<graphene::chain::limit_order_create_operation::limit_order_flags>  order_flags,
                                           uint32_t expiration,
                                           bool   fill_or_kill,
                                           bool   broadcast)
 {
    return my->sell_asset(seller_account, amount_to_sell, symbol_to_sell, min_to_receive,
-                         symbol_to_receive, expiration, fill_or_kill, broadcast);
+                         symbol_to_receive, order_flags, expiration, fill_or_kill, broadcast);
 }
 
 signed_transaction wallet_api::sell( string seller_account,
@@ -3788,8 +3797,9 @@ signed_transaction wallet_api::sell( string seller_account,
                                      double amount,
                                      bool broadcast )
 {
+   fc::optional<graphene::chain::limit_order_create_operation::limit_order_flags>  order_flags;
    return my->sell_asset( seller_account, std::to_string( amount ), base,
-                          std::to_string( rate * amount ), quote, 0, false, broadcast );
+                          std::to_string( rate * amount ), quote, order_flags, 0, false, broadcast );
 }
 
 signed_transaction wallet_api::buy( string buyer_account,
@@ -3799,8 +3809,9 @@ signed_transaction wallet_api::buy( string buyer_account,
                                     double amount,
                                     bool broadcast )
 {
-   return my->sell_asset( buyer_account, std::to_string( rate * amount ), quote,
-                          std::to_string( amount ), base, 0, false, broadcast );
+   fc::optional<graphene::chain::limit_order_create_operation::limit_order_flags>  order_flags;
+	return my->sell_asset( buyer_account, std::to_string( rate * amount ), quote,
+                          std::to_string( amount ), base, order_flags, 0, false, broadcast );
 }
 
 signed_transaction wallet_api::borrow_asset(string seller_name, string amount_to_sell,
